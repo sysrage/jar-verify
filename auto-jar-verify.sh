2ff509b91a957b7ec44143a0730878702e2a554d
Brian Bothwell <sysrage@gmail.com>
2016-02-16 21:23:50 -0500

    Only perform jar-verify on builds with external packages. Otherwise, just extract.

--- auto-jar-verify.sh
+++ auto-jar-verify.sh
@@ -16,48 +16,59 @@ JAR_LASTBUILDSRC="${JAR_BUILDDIR}/${JAR_LASTBUILDNUM}"
 
 for i in $(find ${JAR_BUILDDIR}/* -maxdepth 0 -newer ${JAR_LASTBUILDSRC} -print); do
   JAR_BUILDNUM=${i#${JAR_BUILDDIR}/}
+  if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/ReleaseNotes-${JAR_BUILDNUM}.html" ]; then
+    # Build is complete
+    if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/External/Palau_${JAR_BUILDNUM}_Lenovo_Package.zip" ] || [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" ] || [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" ] || [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" ]; then
+      # Some JAR file packages exist
+      if [ ! -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/" ]; then
+        # Ignore build if dir already exists - otherwise create directory
+        mkdir "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
 
-  if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/ReleaseNotes-${JAR_BUILDNUM}.html" -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" ] || [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" ] || [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" ]; then
-    # Some JARs exist for this build
-    if [ ! -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/" ]; then
-      # Ignore build if dir already exists - otherwise create directory
-      mkdir "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
+        if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/External/Palau_${JAR_BUILDNUM}_Lenovo_Package.zip" ]
+          # Lenovo package exists - unzip JAR files
+          unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/External/Palau_${JAR_BUILDNUM}_Lenovo_Package.zip" *.jar -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
 
-      # Unzip firmware JARs if they exist
-      if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" ]; then
-        unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
+          # Move JARs to base directory and delete extras
+          find "${JAR_WORKDIR}/${JAR_BUILDNUM}/${JAR_BUILDNUM}/" -name '*.jar' -exec mv {} "${JAR_WORKDIR}/${JAR_BUILDNUM}/" \;
+          rm -rf "${JAR_WORKDIR}/${JAR_BUILDNUM}/${JAR_BUILDNUM}/"
+
+          # Run jar-verify against new build
+          ${JAR_NODEBIN} ${JAR_VERIFYBIN} -r ${JAR_RELEASENAME} -b ${JAR_BUILDNUM} > jar-verify-results-${JAR_BUILDNUM}.txt
+
+          # Determine if results are pass or fail
+          JAR_ERRORCOUNT=$(grep 'Finished all activity with' jar-verify-results-${JAR_BUILDNUM}.txt | cut -d ' ' -f 6)
+          if [[ ${JAR_ERRORCOUNT} -eq 0 ]]; then
+            JAR_RESULTS="PASS"
+          else
+            JAR_RESULTS="FAIL"
+          fi
+
+          # E-mail jar-verify results
+          mail -s "JAR Verification Results For ${JAR_RELEASENAME} Build ${JAR_BUILDNUM} -- ${JAR_RESULTS}" "${JAR_EMAILTO}" -- -f ${JAR_EMAILFROM} < jar-verify-results-${JAR_BUILDNUM}.txt
+
+          # Delete jar-verify results
+          rm -f jar-verify-results-${JAR_BUILDNUM}.txt
+        else
+          # Unzip firmware JARs from internal package if it exists
+          if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" ]; then
+            unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_FW_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
+          fi
+
+          # Unzip Linux driver JARs from internal package if it exists
+          if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" ]; then
+            unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
+          fi
+
+          # Unzip Windows driver JARs from internal package if it exists
+          if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" ]; then
+            unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
+          fi
+
+          # Move JARs to base directory and delete extras
+          find "${JAR_WORKDIR}/${JAR_BUILDNUM}/packages/" -name '*.jar' -exec mv {} "${JAR_WORKDIR}/${JAR_BUILDNUM}/" \;
+          rm -rf "${JAR_WORKDIR}/${JAR_BUILDNUM}/packages/"
+        fi
       fi
-
-      # Unzip Linux driver JARs if they exist
-      if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" ]; then
-        unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_LNX_KIT_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
-      fi
-
-      # Unzip Windows driver JARs if they exist
-      if [ -f "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" ]; then
-        unzip -qq "${JAR_BUILDDIR}/${JAR_BUILDNUM}/packages/Internal/Palau_${JAR_BUILDNUM}_LNVOSUPS_WIN_Internal.zip" -d "${JAR_WORKDIR}/${JAR_BUILDNUM}/"
-      fi
-
-      # Move JARs to base directory and delete extras
-      find "${JAR_WORKDIR}/${JAR_BUILDNUM}/packages/" -name '*.jar' -exec mv {} "${JAR_WORKDIR}/${JAR_BUILDNUM}/" \;
-      rm -rf "${JAR_WORKDIR}/${JAR_BUILDNUM}/packages/"
-
-      # Run jar-verify against new build
-      ${JAR_NODEBIN} ${JAR_VERIFYBIN} -r ${JAR_RELEASENAME} -b ${JAR_BUILDNUM} > jar-verify-results-${JAR_BUILDNUM}.txt
-
-      # Determine if results are pass or fail
-      JAR_ERRORCOUNT=$(grep 'Finished all activity with' jar-verify-results-${JAR_BUILDNUM}.txt | cut -d ' ' -f 6)
-      if [[ ${JAR_ERRORCOUNT} -eq 0 ]]; then
-        JAR_RESULTS="PASS"
-      else
-        JAR_RESULTS="FAIL"
-      fi
-
-      # E-mail jar-verify results
-      mail -s "JAR Verification Results For ${JAR_RELEASENAME} Build ${JAR_BUILDNUM} -- ${JAR_RESULTS}" "${JAR_EMAILTO}" -- -f ${JAR_EMAILFROM} < jar-verify-results-${JAR_BUILDNUM}.txt
-
-      # Delete jar-verify results
-      rm -f jar-verify-results-${JAR_BUILDNUM}.txt
     fi
   fi
 done
